f6781dd3857698d4bbe1811abc6e29551051c2fe
Maagio <jmunck.nielsen@gmail.com>
2024-06-13 14:19:29 +0200

    Add files via upload

---
+++ Frontend/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Radiologi___Frontend___Maui.Pages;
+using Radiologi___Frontend___Maui.Services;
+using Radiologi___Frontend___Maui.ViewModel;
+
+namespace Radiologi___Frontend___Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		
+        MainPage = new AppShell();
+    }
+}
---
+++ Frontend/MauiProgram.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using Radiologi___Frontend___Maui.Pages;
+using Radiologi___Frontend___Maui.Services;
+using Radiologi___Frontend___Maui.ViewModel;
+
+namespace Radiologi___Frontend___Maui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<LoginPage>();
+		builder.Services.AddSingleton<LoginViewModel>();
+
+        builder.Services.AddTransient<SignUpPage>();
+        builder.Services.AddTransient<SignUpViewModel>();
+
+        builder.Services.AddSingleton<StudentHomePage>();
+        builder.Services.AddSingleton<StudentHomeViewModel>();
+
+        builder.Services.AddSingleton<TeacherHomePage>();
+        builder.Services.AddSingleton<TeacherHomeViewModel>();
+
+		builder.Services.AddTransient<ClassesPage>();
+		builder.Services.AddTransient<ClassesViewModel>();
+
+        builder.Services.AddSingleton<RankingsPage>();
+        builder.Services.AddSingleton<RankingsViewModel>();
+
+        builder.Services.AddSingleton<TestsOverviewPage>();
+        builder.Services.AddSingleton<TestsOverviewViewModel>();
+
+        builder.Services.AddTransient<CreateTestPage>();
+        builder.Services.AddTransient<CreateTestViewModel>();
+
+        builder.Services.AddTransient<TakeTestPage>();
+        builder.Services.AddTransient<TakeTestViewModel>();
+
+        builder.Services.AddSingleton<UserService>();
+        builder.Services.AddSingleton<TestsService>();
+        builder.Services.AddSingleton<ClassService>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
