e53da5378e06bae3591184699ef65628b62d6794
Maagio <jmunck.nielsen@gmail.com>
2024-06-13 14:20:52 +0200

    Add files via upload

---
+++ Frontend/Models/Class.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Radiologi___Frontend___Maui.Models
+{
+    public class Class
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string ClassName { get; set; }
+        public string ClassCode { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string TeacherId { get; set; }
+    }
+}
